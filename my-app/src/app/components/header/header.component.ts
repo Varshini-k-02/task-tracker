cbe3fa0c578b64edf5affb61a6ca309d9c83c6ae
Varshini <varshinikarthik002@gmail.com>
2023-06-01 21:36:01 +0530

    header

--- my-app/src/app/components/header/header.component.ts
+++ my-app/src/app/components/header/header.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  title:string = 'Task tracker';
+  toggleAddTask(){
+    console.log('toggle');
+  }
 }
