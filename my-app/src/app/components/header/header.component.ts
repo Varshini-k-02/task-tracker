62261e393f7c5692bf878aee7f8035013db51a72
Varshini <varshinikarthik002@gmail.com>
2023-06-09 19:22:26 +0530

    Toggle added

--- my-app/src/app/components/header/header.component.ts
+++ my-app/src/app/components/header/header.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-  title:string = 'Task tracker';
-  toggleAddTask(){
-    console.log('toggle');
+  title: string = 'Task tracker';
+  showAddTask!: boolean;
+  subscription!: Subscription; //Of type Subscription
+  //Subscription is a type of observable, disposale object
+
+  constructor(private uiService: UiService) {
+    //Setting a subscription to the toggle
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
+
+  toggleAddTask() {
+    this.uiService.toggleAddTask();
   }
 }
--- my-app/src/app/components/tasks/tasks.component.ts
+++ my-app/src/app/components/tasks/tasks.component.ts
@@ -26,6 +26,15 @@ export class TasksComponent {
         () => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
       );
   }
+
+  toggleReminder(task:Task){
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
+  
+  addTask(task: Task) {
+    this.taskService.addTask(task).subscribe((task) => this.tasks.push(task));
+  }
 }
 
 //getTasks() is an observable
